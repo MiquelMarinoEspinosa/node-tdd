3f30335c67d23b18963beed35253d134f245299e
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-26 12:35:28 +0000

    refactor unit test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,18 +1,21 @@
-import {describe, expect, it} from '@jest/globals';
+import {beforeEach, describe, expect, it} from '@jest/globals';
 import { PalindromeChecker } from '../src/index';
 
 describe('palindrome checker', () => {
+    let palindromeChecker: PalindromeChecker;
+
+    beforeEach(() => {
+        palindromeChecker = new PalindromeChecker();
+    })
+
     it('exists', () => {
-        let palindromeChecker = new PalindromeChecker();
         expect(palindromeChecker).toBeDefined();
     });
 
     it('should be able to tell when "mom" is a palindrome', () => {
-        let palindromeChecker = new PalindromeChecker();
         expect(palindromeChecker.isAPalindrome('mom')).toBeTruthy();
     });
     it('should be able to tell that "bill" is not a palindrome', () => {
-        let palindromeChecker = new PalindromeChecker();
         expect(palindromeChecker.isAPalindrome('bill')).toBeFalsy();
     });
 });
