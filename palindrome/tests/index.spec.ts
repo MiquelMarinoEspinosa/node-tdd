18fe36f88d151898cd2c1a06e3096df77e815f87
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-26 12:31:51 +0000

    test palindrome and not palindrome

--- palindrome/tests/index.spec.ts
+++ palindrome/tests/index.spec.ts
@@ -4,6 +4,15 @@ import { PalindromeChecker } from '../src/index';
 describe('palindrome checker', () => {
     it('exists', () => {
         let palindromeChecker = new PalindromeChecker();
-        expect(palindromeChecker).toBeDefined()
+        expect(palindromeChecker).toBeDefined();
+    });
+
+    it('should be able to tell when "mom" is a palindrome', () => {
+        let palindromeChecker = new PalindromeChecker();
+        expect(palindromeChecker.isAPalindrome('mom')).toBeTruthy();
+    });
+    it('should be able to tell that "bill" is not a palindrome', () => {
+        let palindromeChecker = new PalindromeChecker();
+        expect(palindromeChecker.isAPalindrome('bill')).toBeFalsy();
     });
 });
